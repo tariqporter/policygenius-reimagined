3de42b26148b0081414c53bfcc5b7593a9cff493
Maisy Chung <maisy.chung@policygenius.com>
2020-09-15 21:11:33 -0400

    Added styled components and existing fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+import GlobalFonts from './fonts/fonts.js';
+
+const Title = styled.div`
+  font-family: 'SharpSans Display No1 Bold', Arial, sans-serif;
+  font-weight: 700;
+  margin: 0;
+  letter-spacing: 0.7px;
+  font-size: ${(props) => props.fontSize};
+`;
+
+const Text = styled.div`
+  font-family: 'SharpSans', Arial, sans-serif;
+  font-weight: 400;
+  margin: 0;
+  letter-spacing: 0.1px;
+  font-size: ${(props) => props.fontSize};
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalFonts />
+
+      <Title fontSize="64px">Shop the market and get insurance right</Title>
     </div>
   );
 }
