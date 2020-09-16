b53ac04bd21bd1616e11e1b82eb19879de1ba275
Tariq Porter <tariqporter@users.noreply.github.com>
2020-09-16 19:58:52 -0400

    Add LARGE_DESKTOP_MEDIA_QUERY

--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -4,3 +4,4 @@ export const LARGE_DESKTOP_BREAKPOINT_PX = 1280;
 
 export const TABLET_MEDIA_QUERY = `only screen and ( min-width: ${TABLET_BREAKPOINT_PX}px )`;
 export const DESKTOP_MEDIA_QUERY = `only screen and (min-width: ${DESKTOP_BREAKPOINT_PX}px)`;
+export const LARGE_DESKTOP_MEDIA_QUERY = `only screen and (min-width: ${LARGE_DESKTOP_BREAKPOINT_PX}px)`;
