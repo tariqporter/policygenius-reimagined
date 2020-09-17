7658bd491102f5b03780413145e74a36446e4d5b
Tariq Porter <tariqporter@users.noreply.github.com>
2020-09-17 12:06:20 -0400

    Add steps

---
+++ src/components/Step.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Text } from 'styledComponents';
+
+const StepStyle = styled.div`
+  display: flex;
+  justify-content; center;
+  align-items: center;
+  background: #fff;
+  box-shadow: 10px 10px 25px 1px rgba(43, 39, 39, 0.25);
+  padding: 12px;
+  width: 380px;
+`;
+
+export const StepNumber = styled(Text)`
+  color: #d84713;
+  font-size: 54px;
+  padding: 16px;
+`;
+
+const Step = (props) => {
+  const { children, ...other } = props;
+  return <StepStyle {...other}>{children}</StepStyle>;
+};
+
+export default Step;
