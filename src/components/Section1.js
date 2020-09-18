aa91fddc740a420153eceaf1640d6fbcea2460d9
Tariq Porter <tariqporter@users.noreply.github.com>
2020-09-18 12:05:37 -0400

    Debounce scroll

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import React, { useRef } from 'react';
+import styled, { css, keyframes } from 'styled-components';
 import { Title, Subtitle } from 'styledComponents';
 import CtaButton from './CtaButton';
 import GridCol from './GridCol';
@@ -7,12 +7,14 @@ import GridRow from './GridRow';
 import { ChevronRight } from 'icons';
 import womanPool from 'assets/woman_by_pool.png';
 import Quotes from 'assets/Quotes.png';
+import useIsVisible from 'hooks/useIsVisible';
 
 const SectionStyle = styled.div`
   height: 800px;
   background: #d84713;
   margin-top: 80px;
   margin-bottom: 50px;
+  overflow: hidden;
 `;
 
 const slideInDown = keyframes`
@@ -26,7 +28,7 @@ const slideInDown = keyframes`
   
     0% {
       opacity: 0;
-      transform: translateY(-100px);
+      transform: translateY(-200px);
     }
   
     60% {
@@ -46,13 +48,41 @@ const slideInDown = keyframes`
       transform: translateY(0);
     }
 `;
+const skew = keyframes`
+    from,
+    60%,
+    75%,
+    90%,
+    to {
+      animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+  
+    0% {
+      opacity: 0;
+      transform-origin: top;
+      transform: rotateX(75deg) rotateY(10deg) rotateZ(-9deg);
+    }
+  
+    60% {
+      opacity: 1;
+    }
+
+    to {
+      transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+    }
+`;
 
 const QuoteImageBorder = styled.div`
   position: absolute;
   height: 600px;
   box-shadow: inset 0 1px 1px 0 hsla(0, 0%, 100%, 0.1), 0 50px 100px -20px rgba(50, 50, 93, 0.25),
     0 30px 60px -30px rgba(0, 0, 0, 0.3);
-  animation: 1.25s ${slideInDown};
+
+  ${({ isVisible }) =>
+    isVisible &&
+    css`
+      animation: 1.25s ${skew} backwards;
+    `};
 `;
 
 const ImageBorder = styled.div`
@@ -62,8 +92,13 @@ const ImageBorder = styled.div`
   width: 450px;
   height: 300px;
   box-shadow: 0 50px 100px -20px rgba(50, 50, 93, 0.25);
-  animation: 1.25s ${slideInDown};
   border: 1px solid #ede8e5;
+
+  ${({ isVisible }) =>
+    isVisible &&
+    css`
+      animation: 1.25s ${slideInDown} backwards;
+    `};
 `;
 
 const ImageStyle = styled.img`
@@ -71,24 +106,33 @@ const ImageStyle = styled.img`
   object-fit: cover;
   object-position: top;
   max-height: 100%;
-  animation: 1.25s ${slideInDown};
+
+  ${({ isVisible }) =>
+    isVisible &&
+    css`
+      animation: 1.25s ${slideInDown} backwards;
+    `};
 `;
 
 const QuoteImageStyle = styled.img`
   max-width: 100%;
   max-height: 100%;
-  animation: 1.25s ${slideInDown};
 `;
 
 const Section1 = (props) => {
   const { ...other } = props;
+  const quoteRef = useRef(null);
+  const imageRef = useRef(null);
+  const quoteVisible = useIsVisible(quoteRef);
+  const imageVisible = useIsVisible(imageRef);
+
   return (
     <SectionStyle {...other}>
-      <QuoteImageBorder style={{ top: 120, right: -350, overflow: 'hidden' }}>
+      <QuoteImageBorder style={{ top: 120, right: -350, overflow: 'hidden' }} ref={quoteRef} isVisible={quoteVisible}>
         <QuoteImageStyle src={Quotes} alt="Quote mockup" style={{ zIndex: '1000' }} />
       </QuoteImageBorder>
-      <ImageBorder style={{ top: 500, right: 500, overflow: 'hidden' }}>
-        <ImageStyle src={womanPool} alt="Woman by Pool" />
+      <ImageBorder style={{ top: 500, right: 500, overflow: 'hidden' }} ref={imageRef} isVisible={imageVisible}>
+        <ImageStyle src={womanPool} alt="Woman by Pool" isVisible={imageVisible} />
       </ImageBorder>
       <GridRow mobile tablet desktop>
         <GridCol mobile={4} tablet={12} desktop={12}>
